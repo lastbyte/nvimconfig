a355b64ec8653691dc57cd689b19676ee2cd075d
nishant <nishant.9c9@gmail.com>
2024-05-02 21:11:12 +0530

    auto-sessions config change

--- lua/powervim/plugins/auto-session.lua
+++ lua/powervim/plugins/auto-session.lua
@@ -4,7 +4,7 @@ return {
     local auto_session = require("auto-session")
 
     auto_session.setup({
-      auto_clean_after_session_restore = true,
+      auto_clean_after_session_restore = false,
       auto_session_suppress_dirs = { "~/Downloads", "~/Documents", "~/Desktop" },
       auto_save_enabled = true,
     })
